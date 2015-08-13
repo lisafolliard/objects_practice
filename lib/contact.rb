238676519938634645927a2d83d7f121afd4f948
Chris Underwood & kendra ash <student@epicodus.com>
2015-08-13 11:57:13 -0700

    add save, clear, and all methods to our class contact with passing unit tests

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,5 +1,5 @@
 class Contact
-  #@@all_contacts = {}
+  @@all_contacts = []
 
   attr_reader(:last_name, :first_name, :job_title, :company)
 
@@ -8,6 +8,18 @@ class Contact
     @first_name = attributes.fetch(:first_name)
     @job_title = attributes.fetch(:job_title)
     @company = attributes.fetch(:company)
+  end
+
+  define_singleton_method(:all) do
+    @@all_contacts
+  end
+
+  define_method(:save) do
+    @@all_contacts.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@all_contacts = []
   end
 
 end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -2,6 +2,9 @@ require('rspec')
 require('contact.rb')
 
 describe(Contact) do
+  before() do
+    Contact.clear()
+  end
 
   describe('#last_name') do
     it('returns the last_name, first_name, job_title, and company') do
@@ -10,6 +13,28 @@ describe(Contact) do
       expect(test_contact.first_name()).to(eq("John"))
       expect(test_contact.job_title()).to(eq("Grand Poo Bah"))
       expect(test_contact.company()).to(eq("The Man"))
+    end
+  end
+
+  describe('#save') do
+    it('adds a contact to the array of saved contacts') do
+      test_contact = Contact.new({:last_name => "Smith", :first_name => "Tom", :job_title => 'Grand Poo Bah', :company => "The Man"})
+      test_contact.save()
+      expect(Contact.all()).to(eq([test_contact]))
+    end
+  end
+
+  describe('.all') do
+    it('is empty at first') do
+      expect(Contact.all()).to(eq([]))
+    end
+  end
+
+  describe('.clear') do
+    it('empties out all of the saved contacts') do
+      Contact.new({:last_name => "Smith", :first_name => "Tom", :job_title => 'Grand Poo Bah', :company => "The Man"}).save()
+      Contact.clear
+      expect(Contact.all()).to(eq([]))
     end
   end
 
