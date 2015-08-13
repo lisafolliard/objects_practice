5baf616a4310cbecb1bd5ad4818d4cbf59133740
Chris Underwood & kendra ash <student@epicodus.com>
2015-08-13 16:48:57 -0700

    adding code for phone class on browser

--- app.rb
+++ app.rb
@@ -19,6 +19,16 @@ get('/contact/form') do
   erb(:contact_new_form)
 end
 
+get('/email_form') do
+  erb(:email_form)
+end
+
+get('/contact/:id') do
+  @contact = Contact.find(params.fetch('id').to_i())
+  erb(:individual_contact)
+end
+
+
 post('/contact') do
   @last_name = params.fetch("last_name")
   @first_name = params.fetch("first_name")
@@ -27,4 +37,14 @@ post('/contact') do
   Contact.new({:last_name => @last_name, :first_name => @first_name, :job_title => @job_title, :company => @company}).save()
   @all_contacts = Contact.all()
   erb(:success)
+end
+
+post('/individual_contact') do
+  @email_address = params.fetch("email_address")
+  @e_type = params.fetch("e_type")
+  @email = Email.new({:email_address => @email_address, :e_type => @e_type})
+  @email.save()
+  @contact = Contact.find(params.fetch('id').to_i())
+  @contact.add_email(@email)
+  erb(:individual_contact)
 end
