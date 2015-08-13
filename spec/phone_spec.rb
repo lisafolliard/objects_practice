764ff62f512c5b3a0f9f27b2a79ef3b22b66c93c
Chris Underwood & kendra ash <student@epicodus.com>
2015-08-13 10:06:33 -0700

    add code for each field of the Phone class for unit testing.

--- spec/phone_spec.rb
+++ spec/phone_spec.rb
@@ -4,9 +4,11 @@ require('phone.rb')
 describe(Phone) do
 
     describe('#area_code') do
-      it('returns the area_code') do
+      it('returns the area_code, last_seven, and type') do
         test_phone = Phone.new({:area_code => 503, :last_seven => 6781999, :type => 'Home'})
         expect(test_phone.area_code()).to(eq(503))
+        expect(test_phone.last_seven()).to(eq(6781999))
+        expect(test_phone.type()).to(eq('Home'))
       end
     end
 end
